ec911940fcfd7e3e720a5ae7a5480d2cb9212592
Oseno <ewaosejosephoseno@gmail.com>
2024-03-25 03:19:26 +0100

    Add files via upload

---
+++ ShaneCyber/Migrations/20240310225927_addOrderTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShaneCyber.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOrderTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Products",
+                newName: "ProductPrice");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Products",
+                newName: "ProductName");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "ProductDescription");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Products",
+                newName: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductPrice",
+                table: "Products",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Products",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductDescription",
+                table: "Products",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "Id");
+        }
+    }
+}
---
+++ ShaneCyber/Migrations/20240311042935_CheckOutSuccessful.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShaneCyber.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckOutSuccessful : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OrderQuantity = table.Column<int>(type: "int", nullable: false),
+                    OrderTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CustomerFullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
